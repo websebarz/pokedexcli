ef69e6a275219f08ee04188c9e9d2fbc4676b29e
websebarz <websebarz@gmail.com>
2025-08-13 09:10:56 -0600

    add pokecache and test suite

--- internal/pokeapi/client.go
+++ internal/pokeapi/client.go
@@ -3,17 +3,20 @@ package pokeapi
 import (
 	"net/http"
 	"time"
+	"github.com/websebarz/pokedexcli/internal/pokecache"
 )
 
 // Client -
 type Client struct {
+	cache pokecache.Cache
 	httpClient http.Client
 }
 
 
 // NewClient -
-func NewClient(timeout time.Duration) Client {
+func NewClient(timeout, cacheInterval time.Duration) Client {
 	return Client{
+		cache: pokecache.NewCache(cacheInterval),
 		httpClient: http.Client{
 			Timeout: timeout,
 		},
