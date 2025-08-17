4ffeb0856e6ff2e75a7f239843efe82c2eadd016
websebarz <websebarz@gmail.com>
2025-08-17 13:10:10 -0600

    add catch command

---
+++ command_catch.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"math/rand"
+	"errors"
+	"fmt"
+)
+
+func commandCatch(cfg *config, args ...string) error {
+	if len(args) != 1 {
+		return errors.New("you must provide a pokemon name")
+	}
+
+	name := args[0]
+	pokemon, err := cfg.pokeapiClient.GetPokemon(name)
+	if err != nil {
+		return err
+	}
+
+	res := rand.Intn(pokemon.BaseExperience)
+
+	fmt.Printf("Throwing a Pokeball at %s...\n", pokemon.Name)
+	if res > 40 {
+		fmt.Printf("%s escaped!\n", pokemon.Name)
+		return nil
+	}
+	
+	fmt.Printf("%s was caught!\n", pokemon.Name)
+
+	cfg.caughtPokemon[pokemon.Name] = pokemon
+
+	return nil
+}
